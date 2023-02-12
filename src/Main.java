cde06b96f17a5e904eaf0eabe4f3199b8abbb6dd
Your Name <ordoananna@gmail.com>
2023-02-12 14:56:48 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 100;
-        int Recharged = 1200;
-        boolean phoneRecharged = true;
-        int bonus = Recharged / 100;
-        if (Recharged < 1000) {
+        int recharged = 1200;
+        int bonus = recharged / 100;
+        if (recharged < 1000) {
             bonus = 0;
         }
-        System.out.println(amount + bonus + Recharged);
+        System.out.println(amount + bonus + recharged);
     }
 }
